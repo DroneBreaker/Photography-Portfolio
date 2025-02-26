b831794871a94bbde014b307c12f5b380e386f40
DroneBreaker <brownierz01@gmail.com>
2025-02-26 16:43:46 +0000

    Add camera icon to the nav

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <header>
         <div class="flex p-4">
-            <img src="#" >
+            <Camera class="w-6 h-6 mt-1"/>
             <h3 class="px-4">
                 <a href="/" class="bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-500 font-medium text-4xl md:text-2xl">
                     Derick Limitless
@@ -27,7 +27,7 @@
 </template>
 
 <script setup>
-
+import { Camera } from 'lucide-vue-next'
 </script>
 
 <style lang="scss" scoped>
