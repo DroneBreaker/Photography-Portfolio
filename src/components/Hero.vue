0e9ff4b83054dbf4da05dad68e5296fd45a697bd
DroneBreaker <brownierz01@gmail.com>
2025-02-26 15:37:42 +0000

    v1

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,10 +1,10 @@
 <template>
     <section class="px-4 p-2 mb-32">
-        <h1 class="text-3xl md:text-7xl font-bold text-center mt-[16%] md:mt-[12%] h-22 mb-4 bg-clip-text text-transparent bg-gradient-to-r from-red-400 to-blue-500">
+        <h1 class="text-3xl md:text-7xl font-bold text-center mt-[20%] md:mt-[12%] h-22 mb-4 bg-clip-text text-transparent bg-gradient-to-r from-red-400 to-blue-500">
             Capturing Life's Beautiful Moments
         </h1>
 
-        <p class="text-center text-xl max-w-2xl mx-auto"> 
+        <p class="text-center text-sm md:text-xl max-w-2xl mx-auto"> 
             Professional photographer specializing in nature, urban landscapes, and portrait photography
         </p>
     </section>
