10ea324e45958111a6aeed0303555cce43984290
DroneBreaker <brownierz01@gmail.com>
2025-02-26 16:48:29 +0000

    Changes to the camera size

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <header>
         <div class="flex p-4">
-            <Camera class="w-6 h-6 mt-1"/>
+            <Camera class="w-8 md:w-7 h-8 md:h-7 mt-1"/>
             <h3 class="px-4">
                 <a href="/" class="bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-500 font-medium text-4xl md:text-2xl">
                     Derick Limitless
